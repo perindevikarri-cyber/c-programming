15af930c00f82a445a454a633d163124836cf63d
perindevi_karri <perindevikarri@gmail.com>
2025-12-07 13:34:08 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,13 @@
+int main()
+{
+	int x;
+	printf("enter x number");
+	scanf("%d",&x);
+switch(x%2==0)
+	case 1:printf("x is even");
+	       break;
+	case 0:printf("x is odd");
+	       break;	
+	return 0;	
+}
+
