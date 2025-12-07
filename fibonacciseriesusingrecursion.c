927e4568d97be54b6503c03c5161f3d58f05d135
perindevi_karri <perindevikarri@gmail.com>
2025-12-07 13:15:10 +0530

    Add files via upload

---
+++ fibonacciseriesusingrecursion.c
@@ -0,0 +1,22 @@
+int fib(int);
+int main()
+{
+	int n,i;
+	printf("enter size of the series");
+	scanf("%d",&n);
+	printf("the fabonacci series is:");
+	for(i=0;i<=n;i++)
+	{
+		printf("%d",fib(i));
+	}
+	return 0;
+}
+int fib(int i)
+{
+	if(i<=1)
+	return i;
+	else
+	return fib(i-1)+fib(i-2);
+}
+
+
