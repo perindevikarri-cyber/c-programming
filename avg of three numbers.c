a9cbdc046cefebda9a82112f0d29ccdec954c855
perindevi_karri <perindevikarri@gmail.com>
2025-12-07 13:07:50 +0530

    Add files via upload

---
+++ avg of three numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,avg;
+	printf("enter three numbers");
+	scanf("%d%d%d",&a,&b,&c);
+	avg=(a+b+c)/3;
+	printf("avg of three numbers%d",avg);
+	return 0;
+}
