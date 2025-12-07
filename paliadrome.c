6cd60b15101605b2a2cc4373fcf6267bfd34b6f5
perindevi_karri <perindevikarri@gmail.com>
2025-12-07 13:23:30 +0530

    Add files via upload

---
+++ paliadrome.c
@@ -0,0 +1,13 @@
+int main()
+{
+	int x;
+	printf("enter x number");
+	scanf("%d",&x);
+switch(x%2==0)
+	case 1:printf("even");
+	       break;
+	case 0:printf("odd");
+	       break;	
+	return 0;	
+}
+
