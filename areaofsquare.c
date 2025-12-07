0ba0db4004603a3974a2a1ffd2677f28a3c4f2da
perindevi_karri <perindevikarri@gmail.com>
2025-12-07 13:07:18 +0530

    Add files via upload

---
+++ areaofsquare.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+    float s, area;
+    printf("Enter side value:");
+    scanf("%f", &s);
+    area = s * s;
+    printf("area = %f", area);
+    return 0;
+}
