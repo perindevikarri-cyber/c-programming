4822db0f893dc69cc87ef5ad93ff4dab1ce0c192
perindevi_karri <perindevikarri@gmail.com>
2025-12-07 13:32:27 +0530

    Add files via upload

---
+++ sum of array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a[100], n, i, sum = 0;
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+   printf("Enter array values");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    printf("Sum of array values = %d", sum);
+    return 0;
+}
